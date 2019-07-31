ccf6e9230d0714d64c103909a07b401f96457528
Christian Zambrana <chrislajas@gmail.com>
2019-07-31 04:12:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,11 +9,11 @@ def map_to_negativize(source_array)
   return new
 end
 
-def map_to_no_change(source_array)
+def map_to_double(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] )
+    new.push( source_array[i] * 2 )
     i += 1
   end
   return new
