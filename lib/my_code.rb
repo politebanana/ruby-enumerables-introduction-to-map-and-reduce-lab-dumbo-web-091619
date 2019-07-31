cadc2e263709def0f631ef26c4f050125d53d574
Christian Zambrana <chrislajas@gmail.com>
2019-07-31 04:11:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -13,7 +13,7 @@ def map_to_no_change(source_array)
   new = []
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * -1 )
+    new.push( source_array[i] )
     i += 1
   end
   return new
